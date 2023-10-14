67f931294b1f93473e4f51f176e14caae4087058
ASUS <ASUS@PARSA>
2023-10-14 22:03:20 +0330

    Add project files.

---
+++ Crypto/DataItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto
+{
+    public class DataItem
+    {
+        public string? key { get; set; }
+        public string? name { get; set; }
+        public double? price { get; set; }
+        public int? rank { get; set; }
+        public string? updated_at { get; set; }
+        public DateTime? created_at { get; set; }
+    }
+
+
+
+
+}
+
---
+++ Crypto/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Crypto;
+using Newtonsoft.Json;
+
+
+await RunInBackground(TimeSpan.FromSeconds(1), () => InitAsync());
+
+async Task RunInBackground(TimeSpan timeSpan, Action action)
+    {
+        var periodicTimer = new PeriodicTimer(timeSpan);
+        while (await periodicTimer.WaitForNextTickAsync())
+        {
+            action();
+        }
+    }
+
+
+
+    async Task InitAsync()
+    {
+    HttpClient httpclient = new HttpClient();
+    string stringAPI = "https://api.wallex.ir/v1/currencies/stats";
+    HttpResponseMessage response = await httpclient.GetAsync(stringAPI);
+    if (response.IsSuccessStatusCode)
+    {
+        string apiresponse = await response.Content.ReadAsStringAsync();
+        ApiResponseWrapper? apirapper = JsonConvert.DeserializeObject<ApiResponseWrapper>(apiresponse);
+        List<DataItem>? dataitems = apirapper.result;
+        foreach (var item in dataitems)
+        {
+            Console.WriteLine($"Key : {item.key}");
+            Console.WriteLine($"Name : {item.name}");
+            Console.WriteLine($"Price  : {item.price}");
+            Console.WriteLine($"Rank :{item.rank}");
+            Console.WriteLine($"updated_at :{item.updated_at}");
+            Console.WriteLine($"created_at : {item.created_at}");
+            double ? price = ((item.price) * 0.005);
+            await Console.Out.WriteLineAsync($"Prediction is : {price}");
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
